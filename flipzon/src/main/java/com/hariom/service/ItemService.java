22041af3ab02efd83d633602cf03a7e9c290d3f7
Hariom Yadav <hariom.yadav@broadcom.com>
2020-04-01 14:04:17 +0530

    for new item also add entry into quantity

--- flipzon/src/main/java/com/hariom/service/ItemService.java
+++ flipzon/src/main/java/com/hariom/service/ItemService.java
@@ -8,12 +8,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.hariom.dao.ItemDao;
 import com.hariom.entity.Item;
+import com.hariom.entity.Quantity;
 
 @Service
 public class ItemService {
 	
 	@Autowired
 	private ItemDao itemDaoObj;
+	
+	@Autowired
+	private QuantityService quantityService;
 	
 	public Collection<Item> getAllItems(){
         return this.itemDaoObj.getAllItems();
@@ -27,12 +31,19 @@ public class ItemService {
         this.itemDaoObj.removeItemById(id);
     }
 	
-	
+	//update old item
 	public void updateItem(Item item){
     	this.itemDaoObj.updateItem(item);
     }
 	
+	//add new item
+	//1. insert into quantity db
+	//2. insert into item db
 	public void insertItem(Item item){
+		
+		Quantity qq = new Quantity(item.getId(), 1);
+		quantityService.insertQuantity(qq);
+		
         this.itemDaoObj.insertItem(item);
     }
 	
