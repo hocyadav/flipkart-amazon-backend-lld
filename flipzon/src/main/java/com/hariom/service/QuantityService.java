12061011be6b676cead61cbdb15fa80451fafd06
Hariom Yadav <hariom.yadav@broadcom.com>
2020-04-02 21:01:47 +0530

    Quantity service method description

--- flipzon/src/main/java/com/hariom/service/QuantityService.java
+++ flipzon/src/main/java/com/hariom/service/QuantityService.java
@@ -21,25 +21,43 @@ public class QuantityService {
 	@Qualifier("fakeDb")
 	private QuantityDao quqQuantityDao;
 	
+	/**
+	 * Get all Quantity
+	 * @return list of Quantity
+	 */
 	public Collection<Quantity> getAllQuantity(){
         return this.quqQuantityDao.getAllQuantity();
     }
 	
+	/**
+	 * Get Quantity by quantity id
+	 * @param id
+	 * @return Quantity
+	 */
 	public Quantity getQuantityById(int id){
         return this.quqQuantityDao.getQuantityById(id); 
     }
 	
-	//update old Quantity
+	/**
+	 * update old Quantity
+	 * @param quan
+	 */
 	public void updateQuantity(Quantity quan) {
 		this.quqQuantityDao.updateQuantity(quan);
 	}
 	
-	//add new Quantity
+	/**
+	 * add new Quantity
+	 * @param quan
+	 */
 	public void insertQuantity(Quantity quan) {
 		this.quqQuantityDao.insertQuantity(quan);
 	}
 	
-	//remove quantity : dont impl in controller component 
+	/**
+	 * remove quantity : don't Implement in controller component
+	 * @param id
+	 */
 	public void removeQuantity(int id) {
 		this.quqQuantityDao.removeQuantityById(id);
 	}
